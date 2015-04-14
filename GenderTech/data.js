c715452f11dab767617422c668459b53e7baef6b
Yasmin Nouh <yasminnouh@dyn-160-39-170-77.dyn.columbia.edu>
2015-04-14 14:45:04 -0400

    Replaced data with name of company, number of engineers and number of female engineers.

--- GenderTech/data.js
+++ GenderTech/data.js
@@ -1,10 +1,13 @@
 var techData = 
-{
-	"key":["flatironhealth","howaboutwe","sailthru","nytnp","paperlesspost","venmo","renttherunway","foursquare","etsy"],
-	"company":["Flatiron Health","HowAboutWe","Sailthru","The New York Times","Paperless Post","Venmo","Rent the Runway","foursquare","Etsy"],
-	"team":["N/A","N/A","N/A","New Products","N/A","N/A","N/A","N/A","N/A"],
-	"num_female_eng":[1,4,5,3,4,5,7,6,19],
-	"num_eng":[7,20,20,21,28,32,32,85,149],
-	"percent_female_eng":[14.29,20,25,14.29,14.29,15.62,21.88,7.06,12.75],
-	"last_updated":["10/21/13", "10/24/13", "4/14/14", "7/25/14", "12/3/13", "10/23/13", "10/13/13", "11/13/13", "10/23/13"]
-}
+[
+          ['Tech Companies in NYC', 'Number of Engineers', 'Number of Women Engineers'],
+          ['Etsy', 149, 19],
+          ['Foursquare', 85, 6],
+          ['Rent the Runway', 32, 7],
+          ['Venmo', 32, 5],
+          ['Paperless Post', 28, 4],
+          ['The New York Times', 21, 3],
+          ['Sailthru', 20, 5],
+          ['HowAboutWe', 20, 4],
+          ['Flatiron Health', 7, 1]
+        ]
