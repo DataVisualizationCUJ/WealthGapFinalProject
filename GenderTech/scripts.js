11deb5d4ece73293531a93e660d61fcf070bb37a
Yasmin Nouh <yasminnouh@dyn-160-39-171-86.dyn.columbia.edu>
2015-05-01 17:40:07 -0400

    Committing scripts file.

---
+++ GenderTech/scripts.js
@@ -0,0 +1,34 @@
+google.load('visualization', '1.0', {'packages':['corechart']});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+      	
+
+        var data = google.visualization.arrayToDataTable(techData);
+
+       	var options = {
+       	width: '600',
+       	height: '600',
+		title: 'More women needed in tech',
+		subtitle: 'Source: https://github.com/triketora/women-in-software-eng',
+		isStacked: true,
+		fontName: 'Cutive',
+		colors: ['#E770FF','#9300BF' ],
+		
+        chartArea:{left:40,top:80, width:"100%",height:"50%"},
+    
+		hAxis :{
+		title: 'NYC Tech Companies',
+		titleTextStyle: {fontSize: 12}, 
+		slantedText: true,
+		slantedTextAngle: 40
+		},
+		vAxis : {
+		title: 'Number of Employees',
+		titleTextStyle: {fontSize: 12}, 
+		}
+		}
+
+        var chart = new google.visualization.ColumnChart(document.getElementById('columnchart_material'));
+
+        chart.draw(data, options);
+      }
