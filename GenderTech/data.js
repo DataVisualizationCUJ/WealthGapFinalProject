e9e54637c5c419f4653bac2603c8ba39dd41848c
Yasmin Nouh <yasminnouh@dyn-160-39-171-161.dyn.columbia.edu>
2015-05-11 18:37:03 -0400

    Commented out all of data.js file.

--- GenderTech/data.js
+++ GenderTech/data.js
@@ -1,14 +1,39 @@
-var techData = 
-([
-          ['Tech Companies in NYC', 'Women Engineers', 'Men Engineers'],
-          ['Etsy', 19, 130],
-          ['Foursquare', 6, 80],
-          ['Rent the Runway', 7, 25],
-          ['Venmo', 5, 27],
-          ['Paperless Post', 4, 24],
-          ['The New York Times', 3, 18],
-          ['Sailthru', 5, 15],
-          ['HowAboutWe', 4, 16],
-          ['Flatiron Health', 1, 6]
-        ]); 
+// var techData = new google.visualization.DataTable();
+// 
+// techData.addColumn('string', 'Tech Companies'); // Implicit domain label col.
+// techData.addColumn('number', 'Women Engineers'); // Implicit series 1 data col.
+// techData.addColumn('number', 'Men Engineers'); // Implicit series 1 data col.
+// techData.addColumn({type:'string', role:'style'}); // annotation role col.
+// 
+// 
+// techData.addRows([
+   // ['Rent the Runway', 21.88, 78.13, "color:['red','green' ]"],
+   // ['Apple', 20, 80, "color:['red','blue' ]"],
+   // ['Google', 17, 83,"color:['red','green' ]"]
+//          
+// ]);
+
+// var techData = 
+// ([
+          // ['Tech Companies in NYC', 'Women Engineers', 'Men Engineers'],
+//      
+          // ['Rent the Runway', 21.88, 78.13, "color:['red','green' ]"],
+          // ['Apple', 20, 80, "color:['red','blue' ]"],
+          // ['Google', 17, 83,"color:['red','green' ]"],
+//          
+//          
+//       
+        // ]); 
         
+// 
+     // ['Rent the Runway', 21.88, 78.13, "colors:['red','green' ]"],
+          // ['Apple', 20, 80, colors:['red','blue' ]],
+          // ['Google', 17, 83,colors:['red','green' ]],
+          // ['LinkedIn', 17, 83, colors:['red','blue']],
+          // ['Venmo', 15.63, 84.38, colors:['red','blue']], 
+          // ['Facebook', 15, 85, colors:['red','blue']],
+          // ['Paperless Post', 14.29, 85.71,colors:['red','green' ]],
+          // ['Etsy', 12.75, 87.25, colors:['red','green' ]],
+          // ['Twitter', 10, 90, colors:['red','blue']],
+          // ['Foursquare', 7.06, 92.94, colors:['red','blue']]
+//          
