4c6072bb1dabf9439a38fcf6d0d20d901269884c
Yasmin Nouh <yn2270@columbia.edu>
2015-05-11 19:33:46 -0400

    Updated scripts.js file

--- FinalPackage/gender_script.js
+++ FinalPackage/gender_script.js
@@ -1,59 +1,71 @@
-google.load('visualization', '1.0', {
-	'packages' : ['corechart']
-});
-google.setOnLoadCallback(drawChartGenderTech);
+google.load('visualization', '1.0', {'packages':['corechart']});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+      	
+var techData = new google.visualization.DataTable();
 
-var techData = 
-([
-          ['Tech Companies in NYC', 'Women Engineers', 'Men Engineers'],
-          ['Etsy', 19, 130],
-          ['Foursquare', 6, 80],
-          ['Rent the Runway', 7, 25],
-          ['Venmo', 5, 27],
-          ['Paperless Post', 4, 24],
-          ['The New York Times', 3, 18],
-          ['Sailthru', 5, 15],
-          ['HowAboutWe', 4, 16],
-          ['Flatiron Health', 1, 6]
-        ]); 
+techData.addColumn('string', 'Tech Companies'); // Implicit domain label col.
+techData.addColumn('number', 'Women Engineers'); // Implicit series 1 data col.
+techData.addColumn({type:'string', role:'style'});
+techData.addColumn('number', 'Men Engineers'); // Implicit series 1 data col.
+techData.addColumn({type:'string', role:'style'}); // annotation role col.
+
+
+techData.addRows([
+   ['Rent the Runway', 22,'color: #51b2e5', 78,  'color:#154866'],
+    ['Venmo', 16,'color:#51b2e5', 84, 'color:#154866'],
+    ['Paperless Post', 14,'color:#51b2e5', 86, 'color:#154866'],
+    ['Etsy', 13,'color:#51b2e5', 87, 'color:#154866'],
+    ['Foursquare', 07,'color:#51b2e5', 93, 'color:#154866'],
+    
+   ['Apple', 20, 'color:#C0C0C0', 80, 'color:#505050'],
+   ['Google', 17,'color:#C0C0C0', 83, 'color:#505050'],
+   ['LinkedIn', 17,'color:#C0C0C0', 83, 'color:#505050'],
+   ['Facebook', 15,'color:#C0C0C0', 85, 'color:#505050'],
+   ['Twitter', 10,'color:#C0C0C0', 90, 'color:#505050']
+ 
+         
+]);
+
+var formatter = new google.visualization.NumberFormat({suffix:'%', fractionDigits:0});
+formatter.format(techData,1); //Applying this formatter to data in column- index 1
+formatter.format(techData,3);
+
+//var secondFormatter = new google.visualization.PatternFormat();
+//secondFormatter.format(techData,3);
+
+       // var data = google.visualization.arrayToDataTable(techData);
+        data = techData;
         
-function drawChartGenderTech() {
+        // var data = new google.visualization.DataTable();
 
-	var data = google.visualization.arrayToDataTable(techData);
 
-	var options = {
-		width : '600',
-		height : '600',
-		title : 'More women needed in tech',
-		subtitle : 'Source: https://github.com/triketora/women-in-software-eng',
-		isStacked : true,
-		fontName : 'Cutive',
-		colors : ['#E770FF', '#9300BF'],
-
-		chartArea : {
-			left : 40,
-			top : 80,
-			width : "100%",
-			height : "50%"
-		},
-
-		hAxis : {
-			title : 'NYC Tech Companies',
-			titleTextStyle : {
-				fontSize : 12
-			},
-			slantedText : true,
-			slantedTextAngle : 40
+       	var options = {
+       	width: '600',
+       	height: '600',
+		title: 'From small companies to large, gender diversity is a problem',
+		// subtitle: 'Source: https://github.com/triketora/women-in-software-eng',
+		isStacked: 'true', 
+		fontName: 'Cutive',
+		legend: { position: "none" },
+		//colors: ['#51b2e5','#154866','#C0C0C0','#505050'],
+		
+        chartArea:{left:80,top:80, width:"95%",height:"50%"},
+    
+		hAxis :{
+		title: 'Gender Breakdown in Tech Giants vs. NYC Companies',
+		titleTextStyle: {fontSize: 12}, 
+		slantedText: true,
+		slantedTextAngle: 40
 		},
 		vAxis : {
-			title : 'Number of Employees',
-			titleTextStyle : {
-				fontSize : 12
-			},
+		title: 'Percent of Employees',
+		titleTextStyle: {fontSize: 12} 
+		//format: '#%'
 		}
-	}
+		}
 
-	var chart = new google.visualization.ColumnChart(document.getElementById('columnchart_material'));
+        var chart = new google.visualization.ColumnChart(document.getElementById('columnchart_material'));
 
-	chart.draw(data, options);
-}
+        chart.draw(data, options);
+      }
